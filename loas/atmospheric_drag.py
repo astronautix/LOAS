2869b10e1d2f668824b400ff69ce2cb48407275d
Titus Senez <titus.senez@polytechnique.edu>
2020-04-24 10:55:09 +0200

    make pooling work

--- loas/atmospheric_drag.py
+++ loas/atmospheric_drag.py
@@ -5,6 +5,24 @@ import random
 import math
 import multiprocessing as mp
 
+def rayTesting(bounding_sphere_radius, particle_mass, dt, speed, nb_particles, mesh, Q, W):
+    torque  = loas.vector.tov(0,0,0)
+    for _ in range(nb_particles):
+        r = bounding_sphere_radius*math.sqrt(random.random())
+        theta = 2*math.pi*random.random()
+        origin = loas.vector.tov(
+            r*math.cos(theta),
+            r*math.sin(theta),
+            -2*bounding_sphere_radius
+        )
+
+        location, _, _, momentum = Particle(origin, speed, particle_mass).getCollisionOnMesh(mesh, Q, W)
+
+        if location is not None:
+            torque += loas.vector.cross(location, momentum/dt)
+    return torque
+
+
 class SparseDrag(loas.Torque):
 
     def __init__(self, satellite, particle_density, satellite_speed, particle_mass, viewer = None, nb_processes = 1):
@@ -12,9 +30,9 @@ class SparseDrag(loas.Torque):
         self.speed = loas.vector.tov(0,0,satellite_speed)
         self.particle_mass = particle_mass
         self.bounding_sphere_radius = np.linalg.norm(satellite.mesh.extents)/2
-        self.particle_rate = particle_density * satellite.dt*satellite_speed * math.pi*self.bounding_sphere_radius**2
+        self.particle_rate = float(particle_density * satellite.dt*satellite_speed * math.pi*self.bounding_sphere_radius**2)
         self.nb_processes = nb_processes
-        self.viewer = viewer
+        self.viewer = None # testing
 
     def getTorque(self):
         nb_particles = max(int(round(random.normalvariate(
@@ -22,38 +40,23 @@ class SparseDrag(loas.Torque):
             sigma = (self.particle_rate)**(1/2)
         ))),0) # uniform distribution of particle in an infinite volume
 
+        nb_particles = int(self.particle_rate) #testing
+
         if self.viewer is not None:
             batch = loas.viewer.CustomBatch()
         else:
             batch = None
 
-        q = mp.Queue()
-
-        if self.nb_processes >= 2:
-            nb_pending_particles = nb_particles
-            process_nb_particles = nb_particles // self.nb_processes + 1
-            processes = []
-            for no_p in range(self.nb_processes):
-                if no_p == self.nb_processes-1:
-                    process_nb_particles = nb_particles - (self.nb_processes-1)*process_nb_particles
-
-                p = mp.Process(
-                    target=self.rayTesting,
-                    args=(process_nb_particles, q, None)
-                )
-                processes.append(p)
-                p.start()
-
-            torque = loas.vector.tov(0,0,0)
-            for p in processes:
-                torque += q.get()
-
-            for p in processes:
-                p.join()
-
-        else:
-            self.rayTesting(nb_particles, q, batch)
-            torque = q.get()
+        torque = sum(mp.Pool().starmap(rayTesting, [(
+            self.bounding_sphere_radius,
+            self.particle_mass,
+            self.satellite.dt,
+            self.speed,
+            nb_particles//self.nb_processes,
+            self.satellite.mesh,
+            self.satellite.Q,
+            self.satellite.getW()
+        )]*self.nb_processes))
 
         if self.viewer is not None:
             batch.add_line(origin=(0,0,0), dir=torque[:,0]/100, color=(255,255,255))
@@ -61,27 +64,6 @@ class SparseDrag(loas.Torque):
 
         return torque
 
-    def rayTesting(self, nb_particles, queue, batch = None):
-        torque  = loas.vector.tov(0,0,0)
-        for _ in range(nb_particles):
-            r = self.bounding_sphere_radius*math.sqrt(random.random())
-            theta = 2*math.pi*random.random()
-            origin = loas.vector.tov(
-                r*math.cos(theta),
-                r*math.sin(theta),
-                -2*self.bounding_sphere_radius
-            )
-
-            location, _, _, momentum = Particle(origin, self.speed, self.particle_mass).getCollisionOnMesh(self.satellite)
-
-            if batch is not None:
-                batch.add_line(origin[:,0], self.speed[:,0])
-                if location is not None:
-                    batch.add_pyramid(location[:,0])
-
-            if location is not None:
-                torque += loas.vector.cross(location, momentum/self.satellite.dt)
-        queue.put(torque)
 
 class Particle:
     """
@@ -99,7 +81,7 @@ class Particle:
         self.speed = speed
         self.mass = mass
 
-    def getCollisionOnMesh(self, satellite):
+    def getCollisionOnMesh(self, mesh, Q, W):
         """
         Computes the collition coordinate of the particle on the satellite's mesh
 
@@ -107,10 +89,10 @@ class Particle:
         :type satellite: loas.Satellite
         """
 
-        origin_sat = satellite.Q.R2V(self.origin)[:,0]
-        dir_sat = satellite.Q.R2V(self.speed)[:,0]
+        origin_sat = Q.R2V(self.origin)[:,0]
+        dir_sat = Q.R2V(self.speed)[:,0]
 
-        locations, index_ray, index_tri = satellite.mesh.ray.intersects_location(
+        locations, index_ray, index_tri = mesh.ray.intersects_location(
             ray_origins=[origin_sat],
             ray_directions=[dir_sat]
         )
@@ -128,12 +110,12 @@ class Particle:
         index_collision = np.argmin(dists)
 
         location = locations[index_collision]
-        normal = satellite.mesh.face_normals[index_tri[index_collision]]
+        normal = mesh.face_normals[index_tri[index_collision]]
 
-        location = satellite.Q.V2R(np.array([[i] for i in location]))
-        normal = satellite.Q.V2R(np.array([[i] for i in normal]))
+        location = Q.V2R(np.array([[i] for i in location]))
+        normal = Q.V2R(np.array([[i] for i in normal]))
 
-        rel_speed = self.speed - loas.vector.cross(satellite.getW(), location)
+        rel_speed = self.speed - loas.vector.cross(W, location)
 
         normal_rel_speed = (np.transpose(normal) @ rel_speed)[0,0]
         if normal_rel_speed > 0:
