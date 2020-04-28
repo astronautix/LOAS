df2931b1368217cf5c0818a45a14f8b9945a0466
titulebolide <senetit@gmail.com>
2020-04-28 19:21:56 +0200

    dynamic plotting

--- examples/plot.py
+++ examples/plot.py
@@ -1,16 +1,30 @@
 import sys
 import os
 sys.path.append(os.path.join(os.getcwd(), ".."))
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import numpy as np
 import trimesh
 import loas
+import threading
 
 dt = 1/25 # Simulation time step
 I0 = np.diag((200,200,200)) # Satellite inertia tensor
 
+ts = [0]
+ws = [0]
+
 def callback(satellite):
-    print(satellite.Q.vec())
+    global ts, ws
+    ts.append(ts[-1]+dt)
+    ws.append(np.linalg.norm(satellite.getW()))
+
+def animate(i):
+    global tx, ws
+    plt.clf()
+    plt.subplot(1,1,1)
+    plt.plot(ts, ws)
 
 # load mesh object and resize it
 mesh = trimesh.load_mesh("./satellite.stl")
@@ -25,7 +39,8 @@ satellite.addParasiteTorque( drag_torque )
 drag_torque.start() # starts the workers that computes the drag torque
 satellite.start() # starts the satellite's simulation
 
-input() # "enter" stops the execution
+ani = animation.FuncAnimation(plt.figure(), animate, interval=dt)
+plt.show()
 
 satellite.stop() # stops the satellite
 drag_torque.stop() # stop drag_torque workers
