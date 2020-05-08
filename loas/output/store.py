9781932b737cd209101a48dda88abbda54ce9e55
Titus Senez <titus.senez@polytechnique.edu>
2020-05-08 16:22:29 +0200

    create output class store

---
+++ loas/output/store.py
@@ -0,0 +1,16 @@
+class Store:
+    def __init__(self):
+        super().__init__()
+        self.data = {}
+    def update(self, t, **kwargs):
+        for key, value in kwargs.items():
+            if key in self.data:
+                self.data[key].append((t,value))
+            else:
+                self.data[key] = [(t, value)]
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def get_keys(self):
+        return list(self.data.keys())
