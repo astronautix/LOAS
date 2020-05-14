733df37a2ec655f938b143f5ff3d5c70bd70df4b
titulebolide <senetit@gmail.com>
2020-05-14 14:32:39 +0200

    new api

--- examples/drag_angle.py
+++ examples/drag_angle.py
@@ -10,12 +10,14 @@ import threading
 # simulation parameters
 dt = 1/25 # Simulation time step
 I0 = np.diag((200,200,200)) # Satellite inertia tensor
-real_density = 10**(-11) #kg.m-3
-part_per_iterations = 10**4
+part_density = 10**(-11) #kg.m-3
+part_mol_mass = 16e-3 #atomic oxygen
+part_per_iterations = 1e5
 sat_speed = 7000
+part_mass = part_mol_mass / 6.022e23
 
 def processAngle(satellite, store, viewer):
-    for angle in np.linspace(0, math.pi/2, 10):
+    for angle in np.linspace(0, math.pi/2, 100):
         print(angle)
         satellite.Q = loas.utils.Quaternion(math.cos(angle/2), math.sin(angle/2), 0, 0)
         satellite.L = loas.utils.vector.tov(0,0,0)
@@ -29,15 +31,11 @@ bounds = np.array(mesh.bounds)
 mesh.apply_translation(-(bounds[0] + bounds[1])/2) # center the satellite (the mass center should be on 0,0)
 mesh.apply_scale(3) # rescale the model
 
-# determine computation values
-comp_number_density = part_per_iterations / (dt*sat_speed * math.pi * (np.linalg.norm(mesh.extents)/2)**2)
-comp_part_mass = real_density / comp_number_density
-
 # define main objects
 viewer =  loas.output.Viewer( mesh )
 store = loas.output.Store()
 satellite = loas.Satellite( mesh, dt, I0 = I0, output=viewer)
-drag_torque = loas.parasite.SparseDrag( satellite, comp_number_density, sat_speed, comp_part_mass, 6, output=store)
+drag_torque = loas.parasite.SparseDrag( satellite, part_density, sat_speed, part_mass, part_per_iterations, 6, output=store)
 satellite.addParasiteTorque( drag_torque )
 
 drag_torque.start()
